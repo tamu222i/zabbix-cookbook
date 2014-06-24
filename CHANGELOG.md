5c16de60b7bc024a438ffdc7e47580fa042c0146
vagrant <vagrant@default.(none)>
2014-06-24 12:48:05 +0000

    berks create

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of zabbix-cookbook/
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'zabbix-cookbook/'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures zabbix-cookbook/'
+long_description 'Installs/Configures zabbix-cookbook/'
+version          '0.1.0'
+
