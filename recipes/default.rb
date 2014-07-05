4caa5eea2543361602e6b1e82a89c8be3bc2be62
tamu222i <t.a.m.u.2.2.2.i@gmail.com>
2014-07-05 07:14:47 +0000

    add requirement

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,13 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# https://www.zabbix.com/documentation/2.2/manual/installation/requirements
+%w(mysql-server httpd php).each do |p|
+  package p do
+    action :install
+  end
+end
+
 # https://www.zabbix.com/documentation/2.2/manual/installation/install_from_packages
 # http://stackoverflow.com/questions/18174557/chef-correct-way-to-load-new-rpm-and-install-package
 remote_file "#{Chef::Config[:file_cache_path]}/zabbix-release-2.2-1.el6.noarch.rpm" do
