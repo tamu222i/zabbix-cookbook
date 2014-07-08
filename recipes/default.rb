1fbe227ca4da799defd9756b53039ae55ad7b1f9
tamu222i <t.a.m.u.2.2.2.i@gmail.com>
2014-07-08 13:00:41 +0000

    add quote

--- recipes/default.rb
+++ recipes/default.rb
@@ -76,7 +76,7 @@ end
   execute f do
     command <<-EOF
       cat #{f} > "#{Chef::Config[:file_cache_path]}/#{filename}.erb"
-      echo "date.timezone = Asia/Tokyo" >> "#{Chef::Config[:file_cache_path]}/#{filename}.erb"
+      echo "date.timezone = \"Asia/Tokyo\"" >> "#{Chef::Config[:file_cache_path]}/#{filename}.erb"
     EOF
     not_if {File.exists?("#{Chef::Config[:file_cache_path]}/#{filename}")}
   end
