7cfa0289e339f6f3503cf45a7669b92f2511daa8
tamu222i <t.a.m.u.2.2.2.i@gmail.com>
2014-07-12 09:49:57 +0900

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,12 @@
 name             'zabbix'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures zabbix-cookbook'
-long_description 'Installs/Configures zabbix-cookbook'
-version          '0.1.1'
+maintainer       'tamu222i'
+maintainer_email 'tamu222i'
+license          'The MIT License (MIT)'
+description      'Chef cookbook for Zabbix.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+%w(centos).each do |os|
+  supports os
+end
 
